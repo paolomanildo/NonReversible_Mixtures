7ab5127936c520851d952e3d866d9752eab989bf
paolomanildo <paolo.m99original@gmail.com>
2026-07-28 18:10:36 +0200

    Update Figure_3.R

--- Figures_1_2_3_6_8_9_10/Figure_3.R
+++ Figures_1_2_3_6_8_9_10/Figure_3.R
@@ -3,7 +3,7 @@ source("plot_functions.R")
 
 ### FIGURE 3 - overfitted mixture simulation
 figure3_top <- cluster_sizes(n = 1000, N = 100, B = 300,
-                             alpha = 1, K = 2, 
+                             alpha = 1, K = 2, K_init = 1,
                              mu0 = 0, lambda0 = 1, sigma2 = 1,
                              sample_from_model = FALSE,
                              sample_data = function(n){
@@ -12,7 +12,7 @@ figure3_top <- cluster_sizes(n = 1000, N = 100, B = 300,
 
 histo(figure3_top$MG)
 histo(figure3_top$NR)
-violin(figure3_top$MG[,1,],figure3_top$NR[,1,])
+violin(figure3_top$MG[,1,],figure3_top$NR[,1,], gg = TRUE)
 
 figure3_bottom <- cluster_sizes(n = 1000, N = 100, B = 300,
                              alpha = 0.1, K = 2, K_init = 1, 
@@ -24,4 +24,4 @@ figure3_bottom <- cluster_sizes(n = 1000, N = 100, B = 300,
 
 histo(figure3_bottom$MG)
 histo(figure3_bottom$NR)
-violin(figure3_bottom$MG[,1,],figure3_bottom$NR[,1,])
+violin(figure3_bottom$MG[,1,],figure3_bottom$NR[,1,], gg = TRUE)
