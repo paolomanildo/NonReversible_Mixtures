1c222a9ec9ce6c00907ef182353814fc987e1714
paolomanildo <paolo.m99original@gmail.com>
2026-07-28 17:51:23 +0200

    Create Figure_10.R

---
+++ Figures_1_2_3_6_8_9_10/Figure_10.R
@@ -0,0 +1,24 @@
+source("simulate_cluster_sizes.R")
+source("plot_functions.R")
+
+### FIGURE 10 - simulation for the Poisson kernel
+figure10_top <- cluster_sizes(n = 1000, N = 100, B = 300,
+                             alpha = 1, K = 3,
+                             kernel = "poisson",
+                             alpha0 = 1, beta0 = 1,
+                             sample_from_model = TRUE)
+
+violin(figure10_top$MG[,1,],figure10_top$NR[,1,], gg = TRUE)
+cloud_plot(figure10_top$MG, gg = TRUE)
+cloud_plot(figure10_top$R, gg = TRUE)
+cloud_plot(figure10_top$NR, gg = TRUE)
+
+figure10_bottom <- cluster_sizes(n = 1000, N = 100, B = 300,
+                                alpha = 0.1, K = 3,
+                                kernel = "poisson",
+                                alpha0 = 1, beta0 = 1,
+                                sample_from_model = TRUE)
+
+violin(figure10_bottom$MG[,1,],figure10_bottom$NR[,1,], gg = TRUE)
+cloud_plot(figure10_bottom$MG, gg = TRUE)
+cloud_plot(figure10_bottom$NR, gg = TRUE)
