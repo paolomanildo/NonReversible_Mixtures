518f2bbcc5e52b5173bc0e6b61c7c1f1e6593d77
paolomanildo <paolo.m99original@gmail.com>
2026-07-28 17:59:32 +0200

    Create Figure_3.R

---
+++ Figures_1_2_3_6_8_9_10/Figure_3.R
@@ -0,0 +1,27 @@
+source("simulate_cluster_sizes.R")
+source("plot_functions.R")
+
+### FIGURE 3 - overfitted mixture simulation
+figure3_top <- cluster_sizes(n = 1000, N = 100, B = 300,
+                             alpha = 1, K = 2, 
+                             mu0 = 0, lambda0 = 1, sigma2 = 1,
+                             sample_from_model = FALSE,
+                             sample_data = function(n){
+                               rnorm(n,2,1)
+                             })
+
+histo(figure3_top$MG)
+histo(figure3_top$NR)
+violin(figure3_top$MG[,1,],figure3_top$NR[,1,])
+
+figure3_bottom <- cluster_sizes(n = 1000, N = 100, B = 300,
+                             alpha = 0.1, K = 2, K_init = 1, 
+                             mu0 = 0, lambda0 = 1, sigma2 = 1,
+                             sample_from_model = FALSE,
+                             sample_data = function(n){
+                               rnorm(n,2,1)
+                             })
+
+histo(figure3_bottom$MG)
+histo(figure3_bottom$NR)
+violin(figure3_bottom$MG[,1,],figure3_bottom$NR[,1,])
