ea2aa52962407e6e14eb51e7ee1cf5a864438e25
paolomanildo <paolo.m99original@gmail.com>
2026-07-28 17:17:35 +0200

    Create Figure_1.R

---
+++ Figures_1_2_3_6_8_9_10/Figure_1.R
@@ -0,0 +1,21 @@
+source("simulate_cluster_sizes.R")
+source("plot_functions.R")
+
+### FIGURE 1
+n <- 2000
+set.seed(1920)
+z <- sample(1:2,n,TRUE,c(0.9,0.1))
+y <- rnorm(n,c(0.9,-0.9)[z],1)
+figure1 <- cluster_sizes(n = 2000, N = 150, B = 100,
+                         alpha = 0.5, K = 2,
+                         mu0 = 0, lambda0 = 1, sigma2 = 1,
+                         sample_from_model = FALSE,
+                         sample_data = function(n){
+                           y
+                         })
+
+spaghetti(apply(figure1$MG,c(1,3), max), title = "Marginal")
+spaghetti(apply(figure1$R,c(1,3), max), title = "Reversible")
+spaghetti(apply(figure1$NR,c(1,3), max), title = "Non Reversible")
+
+violin(apply(figure1$MG,c(1,3), max),apply(figure1$NR,c(1,3), max))
