cd1216657c63b37b00ccd76e85887f4a1e02d6a4
paolomanildo <paolo.m99original@gmail.com>
2026-08-01 16:22:27 +0200

    Update Figure_2.R

--- Figures_1_2_3_6_8_9_10/Figure_2.R
+++ Figures_1_2_3_6_8_9_10/Figure_2.R
@@ -8,10 +8,10 @@ figure2_top <- cluster_sizes(n = 1000, N = 100, B = 300,
                              mu0 = 0, lambda0 = 1, sigma2 = 1,
                              sample_from_model = TRUE)
 
-violin(figure2_top$MG[,1,],figure2_top$NR[,1,], gg = TRUE)
-cloud_plot(figure2_top$MG, gg = TRUE)
-cloud_plot(figure2_top$R, gg = TRUE)
-cloud_plot(figure2_top$NR, gg = TRUE)
+violin(figure2_top$MG[,1,],figure2_top$NR[,1,])
+cloud_plot(figure2_top$MG, gg = FALSE)
+#cloud_plot(figure2_top$R, gg = TRUE)
+cloud_plot(figure2_top$NR, gg = FALSE)
 
 figure2_bottom <- cluster_sizes(n = 1000, N = 100, B = 300,
                                 alpha = 0.1, K = 3,
@@ -19,7 +19,7 @@ figure2_bottom <- cluster_sizes(n = 1000, N = 100, B = 300,
                                 mu0 = 0, lambda0 = 1, sigma2 = 1,
                                 sample_from_model = TRUE)
 
-violin(figure2_bottom$MG[,1,],figure2_bottom$NR[,1,], gg = TRUE)
-cloud_plot(figure2_bottom$MG, gg = TRUE)
-cloud_plot(figure2_bottom$R, gg = TRUE)
-cloud_plot(figure2_bottom$NR, gg = TRUE)
+violin(figure2_bottom$MG[,1,],figure2_bottom$NR[,1,])
+cloud_plot(figure2_bottom$MG, gg = FALSE)
+#cloud_plot(figure2_bottom$R, gg = FALSE)
+cloud_plot(figure2_bottom$NR, gg = FALSE)
