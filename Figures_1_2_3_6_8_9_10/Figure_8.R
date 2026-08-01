faf98537b9a5ab0ecd329465a819ef32c165e38c
paolomanildo <paolo.m99original@gmail.com>
2026-08-01 16:24:21 +0200

    Update Figure_8.R

--- Figures_1_2_3_6_8_9_10/Figure_8.R
+++ Figures_1_2_3_6_8_9_10/Figure_8.R
@@ -11,9 +11,9 @@ figure8_top <- cluster_sizes(n = 1000, N = 100, B = 300,
                              })
 
 violin(figure8_top$MG[,1,],figure8_top$NR[,1,], gg = TRUE)
-cloud_plot(figure8_top$MG, gg = TRUE)
-cloud_plot(figure8_top$R, gg = TRUE)
-cloud_plot(figure8_top$NR, gg = TRUE)
+cloud_plot(figure8_top$MG, gg = FALSE)
+#cloud_plot(figure8_top$R, gg = FALSE)
+cloud_plot(figure8_top$NR, gg = FALSE)
 
 figure8_bottom <- cluster_sizes(n = 1000, N = 100, B = 300,
                                 alpha = 0.1, K = 3,
@@ -24,6 +24,6 @@ figure8_bottom <- cluster_sizes(n = 1000, N = 100, B = 300,
                                 })
 
 violin(figure8_bottom$MG[,1,],figure8_bottom$NR[,1,], gg = TRUE)
-cloud_plot(figure8_bottom$MG, gg = TRUE)
-cloud_plot(figure8_bottom$R, gg = TRUE)
-cloud_plot(figure8_bottom$NR, gg = TRUE)
+cloud_plot(figure8_bottom$MG, gg = FALSE)
+#cloud_plot(figure8_bottom$R, gg = FALSE)
+cloud_plot(figure8_bottom$NR, gg = FALSE)
