7a09b1c9f92f94499a6a2854c506ba98fda73121
paolomanildo <paolo.m99original@gmail.com>
2026-08-01 16:24:52 +0200

    Update Figure_9.R

--- Figures_1_2_3_6_8_9_10/Figure_9.R
+++ Figures_1_2_3_6_8_9_10/Figure_9.R
@@ -1,7 +1,7 @@
 source("simulate_cluster_sizes.R")
 source("plot_functions.R")
 
-### FIGURE 3 - high dimensional example
+### FIGURE 9 - high dimensional example
 figure9 <- cluster_sizes(n = 1000, d = 18, N = 100, B = 500, 
                          alpha = c(4,1,1,1,1), K = 5,
                          mu0 = rep(0,18),
@@ -12,4 +12,4 @@ figure9 <- cluster_sizes(n = 1000, d = 18, N = 100, B = 500,
 
 histo(figure9$MG); curve(dbeta(x,4,4), add = TRUE, col = "gray")
 histo(figure9$NR,"Non-reversible");  curve(dbeta(x,4,4), add = TRUE, col = "gray")
-violin(figure9$MG[,1,],figure3$NR[,1,], gg = TRUE)
+violin(figure9$MG[,1,],figure9$NR[,1,])
