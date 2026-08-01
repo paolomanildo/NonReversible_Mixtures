979c73733b50da6e91ca354d2ae8855f79d79642
paolomanildo <paolo.m99original@gmail.com>
2026-08-01 16:23:02 +0200

    Update Figure_3.R

--- Figures_1_2_3_6_8_9_10/Figure_3.R
+++ Figures_1_2_3_6_8_9_10/Figure_3.R
@@ -2,8 +2,12 @@ source("simulate_cluster_sizes.R")
 source("plot_functions.R")
 
 ### FIGURE 3 - overfitted mixture simulation
-figure3_top <- cluster_sizes(n = 1000, N = 100, B = 300,
-                             alpha = 1, K = 2, K_init = 1,
+figure3_top <- cluster_sizes(B = 500,
+                             N = 100,
+                             n = 1000,
+                             alpha = c(3/2,3/2),
+                             K = 2,
+                             K_init = 1,
                              mu0 = 0, lambda0 = 1, sigma2 = 1,
                              sample_from_model = FALSE,
                              sample_data = function(n){
@@ -11,17 +15,22 @@ figure3_top <- cluster_sizes(n = 1000, N = 100, B = 300,
                              })
 
 histo(figure3_top$MG)
-histo(figure3_top$NR)
-violin(figure3_top$MG[,1,],figure3_top$NR[,1,], gg = TRUE)
+histo(figure3_top$NR, "Non-reversible")
+violin(figure3_top$MG[,1,],figure3_top$NR[,1,])
 
-figure3_bottom <- cluster_sizes(n = 1000, N = 100, B = 300,
-                             alpha = 0.1, K = 2, K_init = 1, 
-                             mu0 = 0, lambda0 = 1, sigma2 = 1,
-                             sample_from_model = FALSE,
-                             sample_data = function(n){
-                               rnorm(n,2,1)
-                             })
+figure3_bottom <- cluster_sizes(B = 500,
+                                N = 100,
+                                n = 1000,
+                                K = 2,
+                                mu0 = 0,
+                                lambda0 = 1,
+                                sigma2 = 1,
+                                alpha = c(.1,.1),
+                                sample_data = function(n){
+                                  rnorm(n,2,1)
+                                })
 
 histo(figure3_bottom$MG)
 histo(figure3_bottom$NR)
-violin(figure3_bottom$MG[,1,],figure3_bottom$NR[,1,], gg = TRUE)
+violin(figure3_bottom$MG[,1,],figure3_bottom$NR[,1,])
+
