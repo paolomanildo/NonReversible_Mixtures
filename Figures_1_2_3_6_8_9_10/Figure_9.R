02e6ab176d16570d8c5035c8cd172d1f28a581bd
paolomanildo <paolo.m99original@gmail.com>
2026-08-01 16:58:57 +0200

    Update Figure_9.R

--- Figures_1_2_3_6_8_9_10/Figure_9.R
+++ Figures_1_2_3_6_8_9_10/Figure_9.R
@@ -2,6 +2,7 @@ source("simulate_cluster_sizes.R")
 source("plot_functions.R")
 
 ### FIGURE 9 - high dimensional example
+set.seed(123)
 figure9 <- cluster_sizes(n = 1000, d = 18, N = 100, B = 500, 
                          alpha = c(4,1,1,1,1), K = 5,
                          mu0 = rep(0,18),
