a5652476b13e4c1e0eb45dc8940553063e6ec70b
paolomanildo <paolo.m99original@gmail.com>
2026-08-05 10:20:13 +0200

    Refactor Figure_4.R for improved clarity and performance
    Refactor code for clarity and performance, including updates to variable assignments, calculations, and plotting functions.

--- Figure_4/Figure_4.R
+++ Figure_4/Figure_4.R
@@ -1,26 +1,117 @@
-library(ggplot2)
-library(patchwork)
+#ANALYSIS
+load("results01.RData")
 
-#load the data
-load("processed_output01.RData")
-
-#set the grid values
 n_grid = round(10^seq(3, 4, length.out = 5))
 d_grid = c(1,3,5)
 grea <- as.matrix(expand.grid(n_grid,d_grid))
-N <- dim(res[[1]])[1] / 8
+N <- (dim(res[[1]])[1] - 8) / 8
+
+#get the quantities of interest
+entropies <- sapply(res, function(x) {
+  sapply(seq_len(NCOL(x)),function(i){
+    cbind(x[seq_len(N),i],
+          x[2*N + seq_len(N),i],
+          x[4*N + seq_len(N),i],
+          x[6*N + seq_len(N),i])
+  }, simplify = "array")
+}, simplify = "array")
+
+n_pred_calls <- sapply(res, function(x) {
+  sapply(seq_len(NCOL(x)),function(i){
+    cbind(x[N + seq_len(N),i],
+          x[3*N + seq_len(N),i],
+          x[5*N + seq_len(N),i],
+          x[7*N + seq_len(N),i])
+  }, simplify = "array")
+}, simplify = "array")
+
+#ESS
+ESS <- sapply(res, function(x) {
+  t(sapply(seq_len(NCOL(x)),function(i){
+    x[8*N + 1:4,i]
+  }, simplify = "array"))
+}, simplify = "array")
+
+sESS <- sapply(res, function(x) {
+  t(sapply(seq_len(NCOL(x)),function(i){
+    x[8*N + 1:4,i] / x[8*N  + 5:8,i] 
+  }, simplify = "array"))
+}, simplify = "array")
+
+#simulate from the real prior
+prior_draws <- sapply(n_grid,rprior,prior_only = TRUE,
+                      N = 1e4, alpha = .1,
+                      simplify = "array")
+
+#compute the TV distances
+
+#get the common support
+x_lim <- range(prior_draws,entropies, na.rm = TRUE)
+x_lim <- x_lim + c(-1,1) * 0.001 * diff(x_lim)
+
+Y_vals <- apply(prior_draws,3,function(y){
+  p0 <- mean(y == 0)
+  c(p0,(1-p0) * density(y[y != 0],from = x_lim[1], to = x_lim[2], n = 512)$y)
+})
+
+TV <- function(x,y){
+  
+  #compute the two densities
+  x_grid <- seq(x_lim[1],x_lim[2],l = 512)
+  p0 <- mean(x == 0)
+  X_val <- (1-p0) * density(x[x != 0],from = x_lim[1], to = x_lim[2], n = 512)$y
+
+  #compute the TV distance with the trapezoidal rule:
+  
+  #save the absolute differences for the continouos part
+  abs_diff <- abs(X_val - y[-1])
+  
+  #return the integral using the trapezoidal rule
+  0.5 * ( abs(p0 - y[1]) + 
+            sum( diff(x_grid) * (head(abs_diff, -1) + tail(abs_diff, -1)) / 2 ))
+  
+}
+
+TVs <- vector("list",NROW(grea))
+for(cc in seq_along(TVs)){
+  
+  #entropy
+  TVs[[cc]]$entropy <- sapply(seq_len(dim(entropies)[2]), function(s) {
+    ccc <- which(n_grid == grea[cc,1])[1]
+    sapply(seq_len(dim(entropies)[1]), function(i){
+      TV(entropies[i,s,cc,],Y_vals[,ccc])
+    })
+  })
+  
+  #n_pred_calls
+  TVs[[cc]]$n_pred_calls <- sapply(seq_len(dim(n_pred_calls)[2]), function(s) {
+    ccc <- which(n_grid == grea[cc,1])[1]
+    sapply(seq_len(dim(n_pred_calls)[1]), function(i){
+      mean(n_pred_calls[i,s,cc,])
+    })
+  })
+  
+}
+
+{
+  X11()
+  par(mfrow = c(5,3), mar = c(.1,2.1,.1,.1))
+  for(i in 1:15){
+    matplot(TVs[[i]]$entropy, type = "l")
+  }
+}
 
 #determine the number of predictive call to reach convergence
 #for each scenario, then at convergence, take 1000 iteration and compute the ESS
 T2C <- function(X){
   
   #get the TV upper quantile of SAM_NR after burn in
-  q99 <- quantile(X$entropy[-(1:1000),4],0.99)
+  q05 <- quantile(X$entropy[-(1:500),4],0.5)
   
   #get the number of predictive calls
   n_calls <- sapply(seq_along(1:4), function(i){
-    n_iter <- which(X$entropy[,i] <= q99)[1]
-    if(is.na(n_iter)) n_iter <- 3000
+    n_iter <- which(X$entropy[,i] <= q05)[1]
+    if(is.na(n_iter)) n_iter <- 1000
     sum(X$n_pred_calls[seq_len(n_iter),i])
   })
   
@@ -29,17 +120,24 @@ T2C <- function(X){
   
 }
 
+#compute the number of predictive calls to convergence
+n_preds_to_conv <- t(sapply(TVs,T2C))
+
+#plot
+library(ggplot2)
+library(patchwork)
+
 #create the data frame
 df <- expand.grid(n = n_grid,d = d_grid,
-                    sampler = c("MG","NR","NUSAMS(0,5,1)","NUSAMS(0,5,1) + NR"))
+                  sampler = c("MG","NR","NUSAMS(0,5,1)","NUSAMS(0,5,1) + NR"))
 df$T2C <- rep(NA,NROW(df))
 for(i in seq_len(NROW(grea))){
   idx <- sapply(1:4,function(j) NROW(grea)*(j-1) + i)
   
   df$T2C[idx] <- T2C(TVs[[i]])
 }
-df$ESS <- c(t(apply(ESS,1:2,median)))
-df$ESSs <- c(t(apply(ESSs,1:2,median)))
+df$ESS <- c(apply(ESS,1:2,mean))
+df$sESS <- c(apply(sESS,1:2,mean))
 
 #time to convergence plots
 p1 <- ggplot(
@@ -60,7 +158,7 @@ p1 <- ggplot(
     labeller = label_bquote(d == .(d))
   ) +
   labs(
-    y = "N° predictives to converge (log scale)",
+    y = "N° predictives to converge\n(log scale)",
     color = NULL,
     shape = NULL
   ) +
@@ -105,7 +203,7 @@ p2 <- ggplot(
   ) +
   labs(
     x = "Sample size (log scale)",
-    y = "ESS per predictive (log scale)",
+    y = "ESS (log scale)",
     color = NULL,
     shape = NULL
   ) +
@@ -133,7 +231,7 @@ p3 <- ggplot(
   df,
   aes(
     x = n,
-    y = ESSs,
+    y = sESS,
     color = sampler,
     shape = sampler,
     group = sampler
@@ -148,7 +246,7 @@ p3 <- ggplot(
   ) +
   labs(
     x = "Sample size (log scale)",
-    y = "ESS per predictive (log scale)",
+    y = "ESS per predictive\n(log scale)",
     color = NULL,
     shape = NULL
   ) +
