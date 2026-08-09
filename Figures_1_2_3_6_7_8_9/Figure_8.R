c13a698a6fcc39340380fd301e75de73e111cefd
paolomanildo <paolo.m99original@gmail.com>
2026-08-09 09:16:44 +0200

    Update Figure 8 to use new cluster_sizes output

--- Figures_1_2_3_6_7_8_9/Figure_8.R
+++ Figures_1_2_3_6_7_8_9/Figure_8.R
@@ -3,7 +3,7 @@ source("plot_functions.R")
 
 ### FIGURE 8 - high dimensional example
 set.seed(123)
-figure9 <- cluster_sizes(n = 1000, d = 18, N = 100, B = 500, 
+figure8 <- cluster_sizes(n = 1000, d = 18, N = 100, B = 500, 
                          alpha = c(4,1,1,1,1), K = 5,
                          mu0 = rep(0,18),
                          sigma2 = 18 * 2,
@@ -11,6 +11,6 @@ figure9 <- cluster_sizes(n = 1000, d = 18, N = 100, B = 500,
                          sample_from_model = TRUE)
 
 
-histo(figure9$MG); curve(dbeta(x,4,4), add = TRUE, col = "gray")
-histo(figure9$NR,"Non-reversible");  curve(dbeta(x,4,4), add = TRUE, col = "gray")
-violin(figure9$MG[,1,],figure9$NR[,1,])
+histo(figure8$MG); curve(dbeta(x,4,4), add = TRUE, col = "gray")
+histo(figure8$NR,"Non-reversible");  curve(dbeta(x,4,4), add = TRUE, col = "gray")
+violin(figure8$MG[,1,],figure8$NR[,1,])
