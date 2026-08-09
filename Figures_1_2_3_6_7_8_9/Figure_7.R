6def56e63b4bb367beb50d12fda9b5ff3fefcd10
paolomanildo <paolo.m99original@gmail.com>
2026-08-09 09:17:26 +0200

    Update Figure 7 with new cluster size calculations

--- Figures_1_2_3_6_7_8_9/Figure_7.R
+++ Figures_1_2_3_6_7_8_9/Figure_7.R
@@ -3,7 +3,7 @@ source("plot_functions.R")
 
 ### FIGURE 7 - simulation from the prior distribution
 set.seed(123)
-figure8_top <- cluster_sizes(n = 1000, N = 100, B = 300,
+figure7_top <- cluster_sizes(n = 1000, N = 100, B = 300,
                              alpha = 1, K = 3,
                              kernel = "partition",
                              sample_from_model = FALSE,
@@ -11,12 +11,12 @@ figure8_top <- cluster_sizes(n = 1000, N = 100, B = 300,
                                numeric(n)
                              })
 
-cloud_plot(figure8_top$MG, gg = FALSE)
-#cloud_plot(figure8_top$R, gg = FALSE)
-cloud_plot(figure8_top$NR, gg = FALSE, title = "Non-reversible")
-violin(figure8_top$MG[,1,],figure8_top$NR[,1,], gg = TRUE)
+cloud_plot(figure7_top$MG, gg = FALSE)
+#cloud_plot(figure7_top$R, gg = FALSE)
+cloud_plot(figure7_top$NR, gg = FALSE, title = "Non-reversible")
+violin(figure7_top$MG[,1,],figure7_top$NR[,1,], gg = TRUE)
 
-figure8_bottom <- cluster_sizes(n = 1000, N = 100, B = 300,
+figure7_bottom <- cluster_sizes(n = 1000, N = 100, B = 300,
                                 alpha = 0.1, K = 3,
                                 kernel = "partition",
                                 sample_from_model = FALSE,
@@ -24,7 +24,7 @@ figure8_bottom <- cluster_sizes(n = 1000, N = 100, B = 300,
                                   numeric(n)
                                 })
 
-cloud_plot(figure8_bottom$MG, gg = FALSE)
-#cloud_plot(figure8_bottom$R, gg = FALSE)
-cloud_plot(figure8_bottom$NR, gg = FALSE, title = "Non-reversible")
-violin(figure8_bottom$MG[,1,],figure8_bottom$NR[,1,], gg = TRUE)
+cloud_plot(figure7_bottom$MG, gg = FALSE)
+#cloud_plot(figure7_bottom$R, gg = FALSE)
+cloud_plot(figure7_bottom$NR, gg = FALSE, title = "Non-reversible")
+violin(figure7_bottom$MG[,1,],figure7_bottom$NR[,1,], gg = TRUE)
